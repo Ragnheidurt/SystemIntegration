7c770193524c7f85d0ecaad47f9e062d82842b5f
Ragnheidur Try <ragnheidurt3@gmail.com>
2023-03-03 09:23:05 +0100

    Step 2, hopefully correct

--- MoonMission.m
+++ MoonMission.m
@@ -1,3 +1,6 @@
+clear all 
+close all
+clc
 %% Step 1
 % GTO & Constants
 r_a = 42164*10^3;
@@ -35,42 +38,6 @@ deltaV_M = abs((V_EM-V_2)-V_txmu);
 
 deltaV_tot = deltaV_E+deltaV_M
 
-%% Step 2
-r_a = 42164*10^3;
-r_p = 7000*10^3;
-a = (r_a+r_p)/2;
-G = 6.674*10^-11;
-mu = 5.97*10^24;
-r = (6371+200)*10^3;
-mu_M = 7.3477*10^22;
-r_m = 1737.4*10^3;
-r_M = (r_m+(100000));
-R_EM = 405500*10^3+r_m+6371e3;
-
-
-V_0 = sqrt((G*mu)/r);
-
-V_esc = sqrt(2)*V_0;
-
-V_2 = sqrt((G*mu_M)/r_M);
-
-V_EM = sqrt((G*mu)/R_EM);
-
-a_tx = (r+R_EM)/2;
-
-% Transfer Velocity Earth
-
-V_txE = sqrt(G*mu*(2/r-1/a_tx));
-
-% Transfer Velocity Moon
-
-V_txmu = sqrt(G*mu*(2/R_EM-1/a_tx));
-
-deltaV_E = V_txE - V_0;
-
-deltaV_M = abs((V_EM-V_2)-V_txmu);
-
-deltaV_tot = deltaV_E+deltaV_M
 
 %% OMG
 
@@ -106,6 +73,31 @@ delta_v_tot = -delta_v_esc + delta_v1 + delta_v2 - v_cm
 
 
 
+%% Step 2 again
+
+RE = 6371e3;
+ra = 35786e3+RE;
+rp = 167e3+RE;
+a = (ra+rp)/2;
+G = 6.674*10^-11;
+M_E = 5.97*10^24;
+M_M = 7.3477*10^22;
+r_m = 1737.4*10^3;
+r_M = (r_m+(100000));
+R_EM = 392508*10^3;
+a_tx = (rp+R_EM)/2;
+
+
+vGTO = sqrt(G*M_E*(2/rp-1/a));
+Vmoon = sqrt((G*M_E)/R_EM);
+vtxe = sqrt(G*M_E*(2/rp-1/a_tx));
+vtxm = sqrt(G*M_E*(2/R_EM-1/a_tx));
+
+vorbitmoon = sqrt((G*M_M)/r_M);
+
+ve = abs(vtxe-vGTO);
+vm = abs(Vmoon-vorbitmoon)-vtxm;
+vtot = ve+vm
 
 
 
